689bd9f57f0be787f69aa058bb864fb1a0722189
amotekun9 <2306220797@qq.com>
2026-08-14 09:26:15 +0800

    删除stm32工程

--- 10-数据传输策略-接收部分/Hardware/MyCAN.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MYCAN_H
-#define __MYCAN_H
-
-void MyCAN_Init(void);
-void MyCAN_Transmit(CanTxMsg *TxMessage);
-uint8_t MyCAN_ReceiveFlag(void);
-void MyCAN_Receive(CanRxMsg *RxMessage);
-
-#endif
--- STM32_CAN/SYSTEM/Timer/Timer.h
+++
@@ -1,6 +0,0 @@
-#ifndef __TIMER_H
-#define __TIMER_H
-#include "sys.h"
-void Timer_Init(void);
-
-#endif
